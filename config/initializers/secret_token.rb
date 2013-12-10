512895f3597c5b154068a83e35466ea672cee600
Patrick Baselier <patrick@kabisa.nl>
2013-12-10 17:06:25 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3WithEmberInRails::Application.config.secret_key_base = '9562887288a269198f12975f274c63fb5916fd4fb9323353db28475e000735e7d1773c9f42974ae1fe69de910136f2b754efc295c20c240433d6e1dbb76a3328'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D3WithEmberInRails::Application.config.session_store :cookie_store, key: '_d3-with-ember-in-rails_session'
